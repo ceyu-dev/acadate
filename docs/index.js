0e54a86ba3d15c384d0e43b0df4c108862a5d2df
verc <u1benedict425@gmail.com>
2024-10-30 13:32:34 +0800

    website

---
+++ docs/index.js
@@ -0,0 +1,17 @@
+async function initializeAndroidDownload() {
+  const githubReleases = await fetch(
+    "https://api.github.com/repos/ceyu-dev/acadate-rn/releases"
+  );
+
+  githubReleases.json().then((releaseInfo) => {
+    const androidDownload = document.getElementById("download-android");
+    const androidVersion = document.getElementById("version-android");
+    androidDownload.setAttribute(
+      "href",
+      releaseInfo[0].assets[0].browser_download_url
+    );
+    androidVersion.textContent = `${releaseInfo[0].tag_name}`;
+  });
+}
+
+initializeAndroidDownload();
